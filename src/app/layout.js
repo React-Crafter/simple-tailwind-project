d9c81331002d0fa35024fd11210363d5261d44af
React-Crafter <abulbasar1725@gmail.com>
2025-01-09 20:19:41 +0600

    first

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,13 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Mth programmer",
   description: "5jeID43bO9CSfnp_p8PVb4BSmxA1aRZEAZajyRx8j8E",
+  keywords: "Mth programmer, Programming team, Mth team's, web Developer, Mth, Programmer",
+  openGraph: {
+    title: "Mth programmer",
+    description: "5jeID43bO9CSfnp_p8PVb4BSmxA1aRZEAZajyRx8j8E",
+    images: ["img/hero-img.png"],
+    url: "https://mth-programmer.netlify.app/"
+  }
 };
 
 export default function RootLayout({ children }) {
