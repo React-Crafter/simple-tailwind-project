e0300ad47a1e759a8c6a04185264f2be83d45aad
React-Crafter <abulbasar1725@gmail.com>
2025-01-10 07:57:59 +0600

    seo

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card({singaleData}) {
     <div>
         <div className='bg-gray-800 transition duration-100 text-center p-8 border-4 border-gray-800 hover:border-indigo-700 rounded-lg
         hover:cursor-pointer'>
-            <img className='w-1/2 mx-auto' src={singaleData.img} alt={`Mth programmer ${singaleData.title}`} />
+            <img className='w-1/2 mx-auto' src={singaleData.img} alt={`Mth programmer ${singaleData.title}`} title={`Mth programmer ${singaleData.title}`} />
             <h4 className='text-lg underline my-5 underline-offset-8'> {singaleData.title} </h4>
             <p> {singaleData.description} </p>
         </div>
